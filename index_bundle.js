4d4c344d1050e062172f7c7915a558b3da45bba3
Eetin <p17dmitry@gmail.com>
2016-07-06 15:59:20 +0300

    update dist files

--- index_bundle.js
+++ index_bundle.js
@@ -53,22 +53,20 @@
 
 	'use strict';
 	
-	var myData = void 0;
 	d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json', function (d) {
-	  myData = d.data;
-	  // render()
-	  // svgRender()
-	  simpleBarChart();
+	  barChart(d.data);
 	});
 	
-	var simpleBarChart = function simpleBarChart() {
+	var barChart = function barChart(myData) {
 	
-	  var margin = { top: 30, right: 30, bottom: 40, left: 50 };
+	  var margin = { top: 50, right: 30, bottom: 100, left: 60 };
 	
-	  var width = 600 - margin.right - margin.left,
-	      height = 400 - margin.top - margin.bottom,
+	  var width = 900 - margin.right - margin.left,
+	      height = 600 - margin.top - margin.bottom,
 	      barWidth = 5,
 	      barOffset = 2;
+	
+	  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 	
 	  var tempColor = void 0;
 	
@@ -80,12 +78,12 @@
 	
 	  var color = d3.scaleLinear().domain([0, myData.length * .33, myData.length * .66, myData.length]).range(['#B58929', '#C61C6F', '#268BD2', '#85992C']);
 	
-	  var tooltip = d3.select('body').append('div').classed('tooltip', true).style('position', 'absolute').style('padding', '0 10px').style('background', 'white').style('display', 'none');
+	  var tooltip = d3.select('body').append('div').classed('tooltip', true).style('position', 'absolute').style('padding', '2px 10px').style('display', 'none').style('border-radius', '5px');
 	
-	  tooltip.append('div').classed('year', true);
 	  tooltip.append('div').classed('gdp', true);
+	  tooltip.append('div').classed('year', true);
 	
-	  var myChart = d3.select('#chart').append('svg').style('background', "#E7E0CB").attr('width', width + margin.right + margin.left).attr('height', height + margin.top + margin.bottom).append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+	  var myChart = d3.select('#chart').append('svg').classed('center-block', true).attr('width', width + margin.right + margin.left).attr('height', height + margin.top + margin.bottom).append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 	  // .style('background', '#C9D7D6')
 	  .selectAll('rect').data(myData).enter().append('rect').style('fill', function (d, i) {
 	    return color(i);
@@ -93,13 +91,14 @@
 	    return xScale(i);
 	  }).attr('height', 0).attr('y', height).on('mouseover', function (d) {
 	
-	    tooltip.transition().style('display', 'block');
+	    tooltip.transition().style('display', 'block').style('opacity', 1);
 	
-	    d3.select('.tooltip .year').html(function () {
-	      return 'Date: ' + d[0];
-	    });
 	    d3.select('.tooltip .gdp').html(function () {
-	      return 'GDP: ' + d[1];
+	      return '$' + d[1] + ' Billion';
+	    }).style('font-size', '1.2em').style('font-weight', 'bold');
+	    d3.select('.tooltip .year').html(function () {
+	      var date = d[0].split('-');
+	      return months[parseInt(date[1]) - 1] + ' ' + date[0];
 	    });
 	
 	    tooltip.style('left', d3.event.pageX - 50 + 'px').style('top', d3.event.pageY - 50 + 'px');
@@ -136,11 +135,37 @@
 	  hAxis(hGuide);
 	  hGuide.attr('transform', 'translate(' + margin.left + ', ' + (height + margin.top) + ')');
 	
-	  var text = d3.select('svg').append('text').text('Gross Domestic Product, USA').attr('transform', function (d) {
-	    console.log(this);
-	    return 'translate(' + ((width - this.getComputedTextLength()) / 2 + margin.left) + ', ' + margin.top + ')';
-	  });
+	  var header = d3.select('svg').append('text').classed('header', true).text('Gross Domestic Product, USA').attr('transform', 'translate(' + (width / 2 + margin.left) + ', ' + margin.top + ')').style('text-anchor', 'middle').style('font-size', '2em');
+	
+	  var gdpText = d3.select('svg').append('text').classed('gdpText', true).text('GDP, Billions of US Dollars').attr('transform', 'translate(' + (margin.left + 20) + ', ' + margin.top + ') rotate(-90)').style('text-anchor', 'end');
+	
+	  var bottomText = d3.select('svg').append('text').classed('bottom-text', true).text('Units: Billions of Dollars Seasonal Adjustment: Seasonally Adjusted Annual Rate Notes: A Guide to the National Income and Product Accounts of the United States (NIPA) - (http://www.bea.gov/national/pdf/nipaguid.pdf)').call(wrap, width).attr('transform', 'translate(' + (width / 2 + margin.left) + ', ' + (height + margin.top + margin.bottom / 3 * 2) + ')').attr('text-anchor', 'middle').attr('width', width);
 	};
+	
+	function wrap(text, width) {
+	  text.each(function () {
+	    var text = d3.select(this),
+	        words = text.text().split(/\s+/).reverse(),
+	        word,
+	        line = [],
+	        lineNumber = 0,
+	        lineHeight = 1.1,
+	        // ems
+	    y = text.attr("y"),
+	        dy = parseFloat(text.attr("dy") || 0),
+	        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+	    while (word = words.pop()) {
+	      line.push(word);
+	      tspan.text(line.join(" "));
+	      if (tspan.node().getComputedTextLength() > width) {
+	        line.pop();
+	        tspan.text(line.join(" "));
+	        line = [word];
+	        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+	      }
+	    }
+	  });
+	}
 
 /***/ }
 /******/ ]);
